1b5b8f20d1b45f174eaeb82dc848c06768bdf66b
Aditya Sahu <adityaonx@gmail.com>
2023-11-11 21:41:44 +0530

    Uplifted state, travel list with state management!

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  return (
+    <div>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackagingList items={items} />
+      <Summary />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>🌴 FarAway 💼</h1>;
+}
+function Form({ onAddItems }) {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+    console.log(newItem);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for 😍 trip? </h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackagingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Summary() {
+  return <footer className="stats"></footer>;
+}
+export default App;
