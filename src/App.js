519d8be91e6820d4b408181e456b88f7af90d565
Aditya Sahu <adityaonx@gmail.com>
2023-11-11 23:53:43 +0530

    Added Initial Footer also with Percentage

--- src/App.js
+++ src/App.js
@@ -101,14 +101,22 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 function Summary({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
   const numItems = items.length;
   const packedItems = items.filter((item) => item.packed === true).length;
-  const percentPacked = (packedItems / numItems) * 100;
+  const percentPacked = Math.round((packedItems / numItems) * 100);
   return (
     <footer className="stats">
       <em>
-        You have {numItems} on your list, and you already packed {packedItems} (
-        {percentPacked}%)
+        {percentPacked === 100
+          ? "You are ready to  ✈️ "
+          : `You have ${numItems} on your list, and you already packed ${packedItems} (${percentPacked}%)`}
       </em>
     </footer>
   );
